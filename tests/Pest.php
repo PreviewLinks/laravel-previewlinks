e77eb7b917c68664ba69d071b19835899870ff97
Logan Craft <hello@logancraft.dev>
2022-12-08 19:52:23 -0500

    Rename from previewify to previewlinks.

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,5 @@
 <?php
 
-use Flowframe\Previewify\Tests\TestCase;
+use PreviewLinks\Tests\TestCase;
 
 uses(TestCase::class)->in(__DIR__);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Flowframe\Previewify\Tests;
+namespace PreviewLinks\Tests;
 
-use Flowframe\Previewify\PreviewifyServiceProvider;
+use PreviewLinks\PreviewLinksServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -13,14 +13,14 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Flowframe\\Previewify\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'PreviewLinks\\Database\\Factories\\'.class_basename($modelName).'Factory'
         );
     }
 
     protected function getPackageProviders($app)
     {
         return [
-            PreviewifyServiceProvider::class,
+            PreviewLinksServiceProvider::class,
         ];
     }
 
